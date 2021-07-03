ab825bd41708db60b1a73a89d373da637f1b5bbc
Juris Gribuska <juris.gribuska@bt.com>
2021-07-03 19:58:50 +0300

    encode done

--- run_length_encoding.rb
+++ run_length_encoding.rb
@@ -1,7 +1,28 @@
+require 'byebug'
+
+
 class RunLengthEncoding
   class << self
     def encode input
-      input
+      char_arr = input.split('')
+      current_letter = ''
+      counter = 0
+      output = []
+
+      char_arr.each_with_index do |letter, index|
+        counter += 1
+        if char_arr[index+1] == letter
+          next
+        else
+          if counter == 1
+            output << "#{letter}"
+          else
+            output << "#{counter}#{letter}"
+          end
+          counter = 0
+        end
+      end
+      output.join
     end
   end
 end
