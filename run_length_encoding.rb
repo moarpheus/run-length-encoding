205077b1eb672484d48b65924dc24653443d7b64
Juris Gribuska <juris.gribuska@bt.com>
2021-07-07 20:14:30 +0300

    not recognizing numbers greater than 10

--- run_length_encoding.rb
+++ run_length_encoding.rb
@@ -25,21 +25,18 @@ class RunLengthEncoding
     end
 
     def decode input
-      char_arr = input.split('')
-      counter = 0
+      char_arr = input.chars
       output = []
-
-      char_arr.each_with_index do |letter, index|
-        counter += 1
-        if char_arr[index+1] == letter
-          next
-        else
-          if counter == 1
-            output << "#{letter}"
-          else
-            output << "#{counter}#{letter}"
+      while(char_arr.size > 0)
+        if(char_arr[0].to_i > 0)
+          char_arr[0].to_i.times do
+            output << char_arr[1]
           end
-          counter = 0
+          char_arr.delete_at(1)
+          char_arr.delete_at(0)
+        else
+          output << char_arr[0]
+          char_arr.delete_at(0)
         end
       end
       output.join
